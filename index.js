171bb94904ba9a7aa3ab850246ba21529818a839
Suriyakanth.S <93136671+suriyakanth1@users.noreply.github.com>
2024-01-22 16:14:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Manual trigger cicd pipeline test...');
+  res.send('Manual trigger cicd pipeline test');
 });
 
 app.listen(port, () => {
