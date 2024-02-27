e0e36118913ebff98a042238fb43ac278181c610
Suriyakanth.S <93136671+suriyakanth1@users.noreply.github.com>
2024-02-27 17:31:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Manual trigger cicd pipeline test on 23th Feb 2024 Time: 10:00');
+  res.send('Manual trigger cicd pipeline test on 23th Feb 2024 Time: 5:00');
 });
 
 app.listen(port, () => {
